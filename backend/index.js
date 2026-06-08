0076b329c748d79cb46d05f7e6747c86e54ec78c
vedantbariar-sudo <vedantbariar@gmail.com>
2026-06-08 19:46:48 +0530

    Fix backend tracking

---
+++ backend/index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+console.log("VEDANT BACKEND FILE STARTED");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    task: String
+});
+
+const Task = mongoose.model("Task", taskSchema);
+mongoose.connect(
+    process.env.MONGO_URI
+);
+console.log("MongoDB connection requested");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/hello", (req, res) => {
+    console.log("HELLO ROUTE HIT");
+    res.send("HELLO FROM BACKEND");
+});
+app.get(
+    "/tasks",
+    async (req, res) => {
+        const data = await Task.find();
+
+        res.json(data);
+    }
+);
+app.post(
+    "/tasks",
+    async (req, res) => {
+
+        console.log("BODY:", req.body);
+
+        await Task.create({
+            task: req.body.task
+        });
+
+        const data = await Task.find();
+
+        console.log("DATA:", data);
+
+        res.json(data);
+    }
+);
+app.delete(
+    "/tasks",
+    async (req, res) => {
+
+        console.log("BODY:", req.body);
+
+        const result = await Task.deleteOne({
+            _id: req.body.id
+        });
+
+        console.log("RESULT:", result);
+
+        const data = await Task.find();
+
+        res.json(data);
+    }
+);
+console.log("DELETE ROUTE VERSION 123456");
+app.listen(5002, () => {
+    console.log("Server running on port 5002");
+});
