5e7be9827b4dc5e93d29adf89dcab832113240cb
vedantbariar-sudo <vedantbariar@gmail.com>
2026-06-23 18:05:31 +0530

    trying to fix again

--- backend/index.js
+++ backend/index.js
@@ -31,35 +31,53 @@ app.get("/hello", (req, res) => {
     res.send("HELLO FROM BACKEND");
 });
 
-function authMiddleware(req, res, next) {
+function authMiddleware(
+    req,
+    res,
+    next
+) {
 
-    const token = req.headers.authorization;
+    const authHeader =
+        req.headers.authorization;
 
-    console.log("TOKEN RECEIVED:", token);
+    if (!authHeader) {
 
-    if (!token) {
         return res.status(401).json({
             message: "No token provided"
         });
+
     }
+
+    const token =
+        authHeader.split(" ")[1];
 
     try {
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        );
+
+        const decoded =
+            jwt.verify(
+                token,
+                process.env.JWT_SECRET
+            );
 
         req.user = decoded;
+
         next();
+
     }
+
     catch(error) {
 
-        console.log("JWT ERROR:", error);
+        console.log(
+            "JWT ERROR:",
+            error
+        );
 
         return res.status(401).json({
             message: "Invalid token"
         });
+
     }
+
 }
 app.get(
     "/tasks", authMiddleware,
